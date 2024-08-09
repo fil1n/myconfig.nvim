c2dba8c6844b56c14c0527abd5d9b31084c51eff
fil1n <22854425+fil1n@users.noreply.github.com>
2024-08-09 20:38:32 +0500

    feat: removed keymap prototype

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -6,8 +6,3 @@ vim.g.mapleader = ','
 
 -- force quit
 keymap.set('n', '<leader>fq', ':qall!<CR>')
-
--- generate tags
-if vim.fn.executable('ctags') == 1 then
-	vim.keymap.set('n', '<leader>ct', ':!ctags --recurse . <CR>')
-end
