2299b63b99d66ea01a1e9bcc75f746855ffe8156
fil1n <22854425+fil1n@users.noreply.github.com>
2024-08-10 21:22:48 +0500

    feat: clear highlights keymap set

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -9,3 +9,6 @@ keymap.set('n', '<leader>fq', ':qall!<CR>')
 
 -- exit insert mode in terminal
 keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- clear highlights
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
