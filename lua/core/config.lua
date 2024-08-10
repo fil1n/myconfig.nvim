03e85366a0ded9e286bf340419500927ca9fa8e4
fil1n <22854425+fil1n@users.noreply.github.com>
2024-08-10 21:23:40 +0500

    feat: new options

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -32,6 +32,8 @@ opt.scrolloff = 5
 -- do not wrap lines
 opt.wrap = false
 
--- set font for nvim-qt
--- https://github.com/tonsky/FiraCode
-pcall(opt.guifont, 'Fira Code')
+-- preview substitutions
+opt.inccommand = 'split'
+
+-- undo history 
+opt.undofile = true
